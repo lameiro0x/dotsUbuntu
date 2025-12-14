5bc28712e9a036c8fcfab6b6f207e472274c8127
Fomo <m.lameiro.2023@alumnos.urjc.es>
2025-12-14 18:16:49 +0100

    Removed emacs config

--- .emacs.d/var/save-place.el
+++
@@ -1,5 +0,0 @@
-;;; -*- coding: utf-8; mode: lisp-data -*-
-(("/home/fomo/Documents/Obsidian/obsidian/Universidad/4º año/Primer Semestre/Estructura de datos avanzada/Tema 5 Grafos/Ejercicios/New/Ejercicio2/src/main/java/es/urjc/grafo/EDA/graph/EMDigraph.java" . 9231)
- ("/home/fomo/Documents/Obsidian/obsidian/Universidad/4º año/Primer Semestre/Sistemas operativos/Practicas/Practica 1/myshell.c" . 226)
- ("/home/fomo/Downloads/a.scala" . 91)
- ("/home/fomo/Documents/Obsidian/obsidian/Universidad/4º año/Primer Semestre/Sistemas operativos/Tema 3 Intro Programacion SOs/Ejercicios/Soluciones/soluciones_ejercicios_tema3/myhead.c" . 330))
